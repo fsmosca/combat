f075acb23bfd26af8b5e91d5686f7798932da9aa
fsmosca <fsmosca@users.noreply.github.com>
2020-02-29 12:57:04 +0800

    Add files via upload

---
+++ run_combat.bat
@@ -0,0 +1,9 @@
+combat.py --engine-config-file combat.json ^
+--engine config-name="Deuterium v2019.2" tc=60000+100 --engine config-name="Deuterium v2019.2 mobility130" tc=60000+100 ^
+--opening file="grand_swiss_2019_6plies.pgn" random=true ^
+--round 8 ^
+--reverse ^
+--win-adjudication score=700 count=4 ^
+--parallel 2
+
+pause
