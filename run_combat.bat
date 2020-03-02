6237ce41502f8b868d087b072aa655f8af9e4dd7
fsmosca <fsmosca@users.noreply.github.com>
2020-03-02 12:27:55 +0800

    Update run_combat.bat
    * Use --engine-log flag to save engine logging in engine_log.txt
    * Use --log-filename option to save combat logging to a different file other than the default combat_log.txt
    * This batch file is applicable for combat v1.14

--- run_combat.bat
+++ run_combat.bat
@@ -1,8 +1,10 @@
 combat.py --engine-config-file combat.json ^
---engine config-name="Deuterium v2019.2" tc=60000+100 --engine config-name="Deuterium v2019.2 mobility130" tc=60000+100 ^
---opening file="grand_swiss_2019_6plies.pgn" random=true ^
+--engine config-name="Deuterium v2019.2 mobility130" tc=5000+50 --engine config-name="Deuterium v2019.2" tc=5000+50 ^
+--opening file="grand_swiss_2019_6plies.epd" random=true ^
 --round 8 ^
 --reverse ^
+--engine-log ^
+--log-filename "mylog.txt" ^
 --win-adjudication score=700 count=4 ^
 --parallel 2
 
