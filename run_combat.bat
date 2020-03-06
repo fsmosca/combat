92594b9abe32d13d92fbeac568890db790cb088f
fsmosca <fsmosca@users.noreply.github.com>
2020-03-06 13:57:28 +0800

    Update run_combat.bat

--- run_combat.bat
+++ run_combat.bat
@@ -1,14 +1,13 @@
 :: Gauntlet match
 
 combat.py --engine-config-file combat.json ^
---engine config-name="Deuterium v2019.2 Elo 2000" tc=5000+50 ^
 --engine config-name="Deuterium v2019.2 Elo 1900" tc=5000+50 ^
 --engine config-name="Deuterium v2019.2 Elo 1800" tc=5000+50 ^
 --engine config-name="Deuterium v2019.2 Elo 1700" tc=5000+50 ^
---opening file="grand_swiss_2019_6plies.pgn" random=true ^
+--opening file="opening/grand_swiss_2019_6plies.pgn" random=true ^
 --round 50 ^
 --reverse ^
---win-adjudication score=700 count=4 ^
---parallel 2
+--win-adjudication score=900 count=3 ^
+--parallel 6
 
 pause
